05ffd91f59f1b880ee58787c0d8e965684b3a47c
RayanaVital <rayanavital@gmail.com>
2025-02-14 16:10:18 -0300

    refactor app structure to use separate route for books

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+
+
+const books = require('./routes/books');
+
+app.use('/books', books);
+
+
 const port = 8000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
