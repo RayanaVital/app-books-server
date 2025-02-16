f3ee44ccd81da1a5fc73b66f94d9ae6ead4370df
RayanaVital <rayanavital@gmail.com>
2025-02-16 15:08:56 -0300

    add CRUD routes for books with error handling

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,24 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello World!');
+  try {
+    res.send('Hello World!');
+
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+router.post('/', (req, res) => {  
+  res.send('Got a POST request');
+});
+
+router.patch('/', (req, res) => { 
+  res.send('Got a PATCH request');
+});
+
+router.delete('/', (req, res) => {
+  res.send('Got a DELETE request');
 });
 
 module.exports = router;
