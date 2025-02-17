3571d0c9923017856c4cb64f8fc099747a85f4d7
RayanaVital <rayanavital@gmail.com>
2025-02-17 16:35:35 -0300

    update getBooks controller to read from books.json and return book data

--- controllers/books.js
+++ controllers/books.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+
+
 function getBooks(req, res) {
     try {
-        res.send('Hello World!');
+        const books = JSON.parse(fs.readFileSync('books.json', 'utf8'));
+        res.send(books);
 
     } catch (e) {
         res.status(500).send(e.message);
