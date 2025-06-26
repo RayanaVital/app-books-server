b105b1d892ef2f40713c08bdd8faeb19fbcc3370
RayanaVital <rayanavital@gmail.com>
2025-06-26 15:04:05 -0300

    add insertFavorite function to handle adding new favorites

--- services/favorites.js
+++ services/favorites.js
@@ -11,9 +11,20 @@ function deleteFavoriteById(id) {
     fs.writeFileSync("favorites.json", JSON.stringify(updatedFavorites));
 }
 
+function insertFavorite(newFavorite) {
+    const books = JSON.parse(fs.readFileSync('books.json', 'utf8'));
+    const favorites = JSON.parse(fs.readFileSync('favorites.json', 'utf8'));
+
+    const insertedBook = books.find(book => book.id === id);
+    const newFavoritesList = [...favorites, insertedBook];
+    
+    fs.writeFileSync('favorites.json', JSON.stringify(newFavoritesList));
+}
+
 
 
 module.exports = {
     getAllFavorites,    
-    deleteFavoriteById
+    deleteFavoriteById,
+    insertFavorite
 }
