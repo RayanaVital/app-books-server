2b813c14fe8c2fd6c1e3b01e41c64f4d8995a85e
RayanaVital <rayanavital@gmail.com>
2025-06-26 14:54:58 -0300

    add deleteFavoriteById function to manage favorite entries

--- services/favorites.js
+++ services/favorites.js
@@ -4,6 +4,16 @@ function getAllFavorites() {
     return JSON.parse(fs.readFileSync('favorites.json', 'utf8'));
 }
 
+
+function deleteFavoriteById(id) {
+    const favorites = JSON.parse(fs.readFileSync("favorites.json", "utf8"));
+    const updatedFavorites = favorites.filter(favorite => favorite.id !== id);
+    fs.writeFileSync("favorites.json", JSON.stringify(updatedFavorites));
+}
+
+
+
 module.exports = {
     getAllFavorites,    
+    deleteFavoriteById
 }
